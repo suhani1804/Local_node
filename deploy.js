f489bf7f05d0f286f84daefdee6301232f8bee5f
SuhaniMohanty <107800177+suhani1804@users.noreply.github.com>
2023-04-07 00:38:33 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,51 +3,57 @@ const fs = require("fs-extra");
 
 async function main() {
 
-  // DEPLOYED OUR CONTRACT TO THE GANACHE BLOCKCHAIN
+  // DEPLOYED OUR CONTRACT TO THE TESTNET
 
   // In provider, provide your rpc url to connect to the blockchain
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"//it is the rpc url of the ganache network
+    "http://127.0.0.1:7545"// e.g it is the rpc url of the ganache chain
   );
 
 
-  //Wallet is from where we want to transfer the ether 
+  //Wallet is from where we want to transfer the ether so need to provide info about private key of the wallet 
   const wallet = new ethers.Wallet(
-    "0xcba006412cb9d3648025e0a8265140dfce0380503a55049c0e1d54f814a5e402",//it is private key of the account that we are using in ganache
-    provider
+    "0xcba006412cb9d3648025e0a8265140dfce0380503a55049c0e1d54f814a5e402", //e.g it is private key of the address that we are using in ganache
+    provider //provider of network is required to get wallet
   );
 
-
-  //from fs / fs-extra we can get the abi and binary compiler
+  // abi and bin we get after deploying solidity smart contract 
+  // abi and binary code re main requirememnt to interact to any smart contract
+  
+  //from fs / fs-extra we can get the abi and binary compiler 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
-    // readFileSync("path of the abi", "utf-8 for encoding the abi")
+  // readFileSync("path of the abi", "utf-8 for encoding the abi")
 
 
   const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin","utf-8");
 
   // In ethers contract factory is just an object that is used to deploy the contract
-  
+  // As we are deploying our smart contract we must have abi and bin to interract and also the wallet address where we want to deploy it.
   const contractfactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying, please wait...");
   // contractfactory comes with all the functionality we have in abi
-  const contract = await contractfactory.deploy(); // deplying the contract , wait for the contract to complete
+  
+  // YAAAHHH! deplying the contract , wait for the deployment to complete
+  const contract = await contractfactory.deploy(); 
   console.log(contract);
-  // contract deployed to te ganache test blockchain
+  // contract deployed to the testnet  e.g ganache testnet
 
-  // we can get the address of the contract that we deployed and hash of block created
-  const address = contract.address;// address of the created contract address 
+  // we can get the address of the deployed contract and hash of block created
+  const address = contract.address;// address of the deployed contract 
   console.log(address);
-  console.log(contract.deployTransaction.hash);// hash of the transaction that we can get from ganache blockchain
+  
+  console.log(contract.deployTransaction.hash);// hash of the block created e.g that we get from ganache 
   // deploy transaction or transaction response is just what you get creating a block on the blockchain
   console.log("Deployed Transaction ", contract.deployTransaction);
 
 
-  // TransactionReceipt is what what you get when you wait for blockconformation
+  // TransactionReceipt is the receipt that you get when you wait for blockconformation
   const TransactionReceipt = await contract.deployTransaction.wait(1);// wait(1) means we wait for 1 block conformation to make sure that the transaction is completed
   console.log(TransactionReceipt); 
   
   //Let's create our customised Deployment data
   console.log("Our custom deployment")
+  // As nonce changes after every transaction, we can call getTransactionCount to get changed nonce
   const nonce = await wallet.getTransactionCount();
   const tx ={
     nonce:nonce,
@@ -66,13 +72,15 @@ async function main() {
 
   // In the above two line we only signed the transaction not send to the blockchain 
   // Now we send the transaction to the blockchain 
-  // And also in sendTransaction it first sign the transaction ad then send it to the blockchain
+  // And also in sendTransaction internally it first sign the transaction and then send it to the blockchain
   const send = await wallet.sendTransaction(tx);
   await send.wait(1);
   console.log("YAHHHH! Transaction sent ",send);
 
+  
   //INTERRACT WITH THE CONTRACT
 
+  
   // retrive is a value returning function so don't have to deploy the function to access it
   const favnumber = await contract.retrieve();
   console.log("Favourite number is ",favnumber.toNumber());
